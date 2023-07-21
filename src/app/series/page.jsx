f171a63a9d7d5965d747dfbf0d10651f20e2e271
emreates2 <emre.ates@trailblu.com>
2023-07-21 15:32:00 +0300

    serie page

--- src/app/series/page.jsx
+++ src/app/series/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
-function SeriesPage() {
+import {
+  getTopRatedSeries,
+  getPopularSeries,
+  getSeriesAiringToday,
+  getSeriesOntheAir,
+} from "@/services/series.service";
+import HomeContainer from "@/containers/home";
+
+async function Series() {
+  const topRatedSeriesPromise = getTopRatedSeries();
+  const popularSeriesPromise = getPopularSeries();
+  const seriesAiringTodayPromise = getSeriesAiringToday();
+  const seriesOntheAirPromise = getSeriesOntheAir();
+
+  const [
+    { results: topRatedSeries },
+    { results: popularSeries },
+    { results: seriesAiringToday },
+    { results: seriesOntheAir },
+  ] = await Promise.all([
+    topRatedSeriesPromise,
+    popularSeriesPromise,
+    seriesAiringTodayPromise,
+    seriesOntheAirPromise,
+  ]);
+
   return (
-    <div>SeriesPage</div>
-  )
+    <HomeContainer
+      topRatedSeries={topRatedSeries}
+      popularSeries={popularSeries}
+      seriesAiringToday={seriesAiringToday}
+      seriesOntheAir={seriesOntheAir}
+    />
+  );
 }
 
-export default SeriesPage
+export default Series;
